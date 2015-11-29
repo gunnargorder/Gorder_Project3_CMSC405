778558890e623e1f0f9ce93c5df4881cb0af4bca
Gunnar Gorder <gunnargorder@gmail.com>
2015-11-28 18:08:23 -0800

    changed scaling to exponetial

--- scaling.cpp
+++ scaling.cpp
@@ -16,23 +16,28 @@ void Scaling::transform(ConvexPolygon * polygon) const
 	GLdouble x2 = 0;
 	GLdouble y2 = 0;
 
+	if((this->startStep!=0) && (this->endStep!=0)){
+		//if currentStep is < starting step no transformation
+		if (currentStep < this->startStep) {
+			return;
+		} 
+		else 	if ((currentStep >= this->startStep) && (currentStep <= this->endStep)) {
+			//if current step is greater than starting step and less than ending step calculate transformation
 
-	//if currentStep is < starting step no transformation
-	if (currentStep < this->startStep) {
-		return;
-	} 
-	else 	if ((currentStep >= this->startStep) && (currentStep <= this->endStep)) {
-		//if current step is greater than starting step and less than ending step calculate transformation
-		//rotAngle = this->angle * (currentStep - this->startStep + 1);
-		x2 = this->x * (currentStep - this->startStep + 1);
-		y2 = this->y * (currentStep - this->startStep + 1);
+			x2 = pow(this->x, (currentStep - this->startStep + 1));
+			y2 = pow(this->y, (currentStep - this->startStep + 1));
+		}
+		else if (currentStep > this->endStep) {
+			//if currentStep is > than ending step, apply final transformation
+
+			x2 = pow(this->x, (this->endStep - this->startStep + 1));
+			y2 = pow(this->y, (this->endStep - this->startStep + 1));
+
+		}
 	}
-	else if (currentStep > this->endStep) {
-		//if currentStep is > than ending step, apply final transformation
-		//rotAngle = this->angle * (this->endStep - this->startStep + 1);
-		x2 = this->x * (this->endStep - this->startStep + 1);
-		y2 = this->y * (this->endStep - this->startStep + 1);
-
+	else {
+		x2 = this->x;
+		y2 = this->y;
 	}
 	polygon->scale(x2, y2);
 }
